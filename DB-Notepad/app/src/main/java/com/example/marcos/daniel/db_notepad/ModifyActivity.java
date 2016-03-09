3275e54435a2fd46b4fca12376f5477f5fb75192
Daniel Marcos <danielmarcos.to@gmail.com>
2016-03-09 04:45:55 +0100

    Update Modify Activity for test, no funcionality

--- DB-Notepad/app/src/main/java/com/example/marcos/daniel/db_notepad/ModifyActivity.java
+++ DB-Notepad/app/src/main/java/com/example/marcos/daniel/db_notepad/ModifyActivity.java
@@ -1,4 +1,23 @@
 package com.example.marcos.daniel.db_notepad;
 
-public class ModifyActivity {
+import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class ModifyActivity extends Activity {
+
+    private EditText etText;
+
+    private Button bInsert;
+    private Button bDiscard;
+
+    private SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify);
+    }
 }
